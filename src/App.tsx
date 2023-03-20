cc083527e2c20c8188ed230894a10b14b6bcb703
julienjamet <julien.jamet.34@gmail.com>
2023-03-20 10:20:19 +0100

    modifs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { ChangeEvent, FC, useState } from 'react';
-import './App.css';
-import TodoTask from './Components/TodoTask';
-import { ITask } from './Interfaces';
+import { ChangeEvent, FC, useState } from 'react'
+import './App.css'
+import { TodoTask } from './Components/TodoTask'
+import { ITask } from './Interfaces'
 
-const App: FC = () => {
+export const App: FC = () => {
 
   const [task, setTask] = useState<string>("")
   const [deadline, setDeadline] = useState<number>(0)
@@ -37,20 +37,22 @@ const App: FC = () => {
 
   return (
     <div className="App">
+
       <div className="header">
+
         <div className='inputContainer'>
           <input type="text" value={task} placeholder='Task...' name="task" onChange={handleChange} />
           <input type="number" value={deadline} onChange={handleChange} />
         </div>
+
         <button onClick={addTask}>Add task</button>
       </div>
+
       <div className="todoList">
         {todoList.map((task: ITask, key: number) => {
           return <TodoTask key={key} task={task} completeTask={completeTask} />
         })}
       </div>
     </div>
-  );
+  )
 }
-
-export default App;
